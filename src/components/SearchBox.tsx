acbb952ef83b87ee9c3aa5fe396d0c8781aad93d
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2018-12-21 03:41:10 +0800

    moved unnecessary not global files to their container component

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -12,13 +12,11 @@ import * as React from 'react';
 // }
 
 const SearchBox: React.SFC<any> = (props) => {
-  let { searchfield } = props;
   return (
     <div className='pa2'>
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
-        value={searchfield}
         placeholder='search robots'
         {...props}
       />
