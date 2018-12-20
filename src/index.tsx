bd694f85b529297adba0c0a358b826b0593c3e76
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2018-12-21 03:42:36 +0800

    wrap the App with a Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import { BrowserRouter as Router, BrowserRouter } from 'react-router-dom'
+import './css/rootIndex/index.css';
+import App from './rootApp/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root') as HTMLElement);
 serviceWorker.unregister();
