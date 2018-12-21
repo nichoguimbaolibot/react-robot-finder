91a21383ef87318646ec710a5973ffa5ea9c6dcf
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2018-12-21 16:31:37 +0800

    modify routes for public routes

--- src/rootApp/App.tsx
+++ src/rootApp/App.tsx
@@ -3,14 +3,13 @@ import { Switch, Route, Link } from 'react-router-dom';
 import 'tachyons'
 import { routes as routesArray } from './Routes'
 import { IRoute } from '../types/rootApp/index'
-import Homepage from '../containers/Homepage'
 
 
 const routes = routesArray()
 
 export default function App(): JSX.Element {
   
-  const routesMap: Array<JSX.Element> =
+  const PublicRoutes: Array<JSX.Element> =
     routes.map(({path, Component, exact}) => {
     return <Route
               key={path}
@@ -26,8 +25,7 @@ export default function App(): JSX.Element {
   return (
     <React.Suspense fallback={<h1>Loading...</h1>}>
     <Switch>
-      {routesMap}
-      <Route path="/" exact={true} component={Homepage} />
+      {PublicRoutes}
     </Switch>
     </React.Suspense>
   )
--- src/rootApp/Routes.tsx
+++ src/rootApp/Routes.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react'
+import Homepage from '../containers/Homepage'
 
 const RoboFinder = React.lazy(() => import('../containers/RoboFinder'))
 
 export const routes = () => [
-  {
+    {
       path: '/robofinder',
       Component: RoboFinder,
+      exact: true,
+    },
+    {
+      path: '/',
+      Component: Homepage,
       exact: true,
     }
   ]
