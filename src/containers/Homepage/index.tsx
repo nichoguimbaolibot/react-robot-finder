6f3cea8e32425347f3e161e03f2f0f7ea163ac5e
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2018-12-21 14:26:08 +0800

    added a homepage

--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -1,11 +1,26 @@
-// import * as React from 'react'
+import * as React from 'react'
+import { Link } from 'react-router-dom'
 // import LinkList from './components/LinkList'
-// const Homepage: React.SFC<JSX.Element> = () => {
-//   return (
-//     <React.Fragment>
-//       <LinkList />
-//     </React.Fragment>
-//   )
-// }
 
-// export default Homepage
+const Homepage: React.SFC<JSX.Element> = () => {
+  return (
+    <section className="homepage-section">
+      <div className="link-list">
+      <ul className="unordered-link-list">
+        <li className="link-item">
+          <Link to="/robofinder">
+            RoboFinder
+          </Link>
+        </li>
+        <li className="link-item">
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+      </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Homepage
