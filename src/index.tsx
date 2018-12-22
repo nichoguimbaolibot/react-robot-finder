0ac044785d6539168669e9421413208a9165c57a
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2018-12-22 20:17:25 +0800

    added setup for reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, compose } from 'redux';
 import { BrowserRouter as Router, BrowserRouter } from 'react-router-dom'
 import './css/rootIndex/index.scss';
 import App from './rootApp/App';
+import { getSearchField } from './reducers'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
+function configureStore(initialState?: object) {
+  // const middlewares = [epicMiddleware];
+
+
+  // return createStore(getSearchField, initialState!)
+}
+
+const Main: React.SFC<any> = () => {
+  return (
+  // <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  // </Provider>
+  )
+}
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Main />,
   document.getElementById('root') as HTMLElement);
 serviceWorker.unregister();
